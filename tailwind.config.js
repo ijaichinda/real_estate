9fb857963d6563ada7fbb34b0ccd850868ebe598
ijai <chindaijai49@gmail.com>
2026-03-04 23:31:30 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily : {
+        quick : "Quicksand",
+        inter: "Inter",
+        manr: "Manrope"
+      }
+    },
+  },
+  plugins: [],
+}
